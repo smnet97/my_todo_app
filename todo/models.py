3d0138d2927393be8e97fc2aa18016e35585e774
smnet97 <smnet97@gmail.com>
2023-06-16 14:53:24 +0500

    added TodoModel

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class TodoModel(models.Model):
+    task_name = models.CharField(max_length=255)
+    description = models.TextField()
+    task_status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.task_name
+
+    class Meta:
+        verbose_name = 'task'
+        verbose_name_plural = 'tasks'
+        ordering = ['-created_at']
