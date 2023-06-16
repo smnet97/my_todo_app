47a145d22973960c197af57ed11f3bb3a6dd21d8
smnet97 <smnet97@gmail.com>
2023-06-16 16:04:09 +0500

    added todo list view

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import todo_list_view
+from .views import todo_list_view, todo_create_view
 
 urlpatterns = [
-    path('', todo_list_view, name='todo_list')
+    path('', todo_list_view, name='todo_list'),
+    path('todo/create/', todo_create_view, name='todo_create')
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CreateTodoModelForm
+from .models import TodoModel
 
 
 def todo_list_view(request):
-    return render(request, 'main/todo_list.html')
+    todos = TodoModel.objects.all()
+    return render(request, 'main/todo_list.html', context={
+        'tasks': todos
+    })
 
 
-def create_todo_view(request):
-    pass
+def todo_create_view(request):
+    form = CreateTodoModelForm()
+    if request.method == 'POST':
+
+        form = CreateTodoModelForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('todo_list')
+
+    return render(request, 'main/todo_create.html', context={
+        'create_form': form
+    })
