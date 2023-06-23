072b0c312345de9ab17c5ab1e95a90bf6a0c7696
smnet97 <smnet97@gmail.com>
2023-06-23 14:14:55 +0500

    added user app

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import TodoModel
 
+
+@admin.register(TodoModel)
+class TodoModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'task_name', 'created_at', 'task_status']
+    list_display_links = ['id', 'task_name', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['task_name']
+    
 
